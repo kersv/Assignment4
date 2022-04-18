2964203009e1e4dd16c709e94c314d9b0ecc7ffc
Kevin <kersv@Kevins-MBP.fios-router.home>
2022-04-18 16:42:24 -0400

    addCredit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './components/Home';
 import UserProfile from './components/UserProfile';
 import LogIn from './components/Login';
+import Credits from './components/Credits';
+import Debits from './components/Debits';
+
+import axios from "axios";
 
 class App extends Component{
   constructor(){
@@ -18,6 +22,31 @@ class App extends Component{
       debits:[]
     }
   }
+  
+
+  async componentDidMount() {
+    let credits = await axios.get("https://moj-api.herokuapp.com/credits")
+    let debits = await axios.get("https://moj-api.herokuapp.com/debits")
+
+    credits = credits.data
+    debits = debits.data
+
+    let sumDebit = 0;
+    let sumCredit = 0;
+
+    credits.forEach((credit) => {
+      sumCredit += credit.amount
+    })
+
+    debits.forEach((debit) => {
+      sumDebit += debit.amount
+    })
+
+    let accountBalance = parseFloat((sumCredit - sumDebit).toFixed(2))
+
+    this.setState({credits,debits,accountBalance})
+  }
+
 
   mockLogIn = (logInInfo) => {
     const newUser = {...this.state.currentUser}
@@ -27,13 +56,30 @@ class App extends Component{
 
 
   addCredit = (e) => {
-    
+    e.preventDefault();
+    const description = e.target[0].value;
+    const amount = Number(e.target[1].value);
+
+    const curr_date = new Date();
+    let date = curr_date.getFullYear() + "-" + curr_date.getMonth() + "-" + curr_date.getDate();
+
+    const cred_obj = {'description':description, 'amount':amount, 'date':date};
+    this.setState({
+      credits:[...this.state.credits, cred_obj]
+    })
+
+    const new_Balance = this.state.accountBalance += amount;
+    this.setState({
+      accountBalance: new_Balance
+    })
   }
   
   addDebit = (e) => {
 
   }
   render(){
+    const {debits} = this.state;
+    const {credits} = this.state;
     const HomeComponent = () => (<Home accountBalance={this.state.accountBalance}/>);
     const UserProfileComponent = () => (<UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince}/>);
     const LogInComponent = () => (<LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn}/>)
